791058206f0d0e1a0c22600908113077ebebad69
Mognor <antonyflores779@outlook.com>
2020-11-23 23:58:49 -0600

    Create new footer

--- App.js
+++ App.js
@@ -1,24 +1,81 @@
 import 'react-native-gesture-handler';
-import React from 'react';
-import inicio from './src/screen/inicio';
+import React, {Component} from 'react';
+import Podcast from './src/screens/podcast';
+import { StyleSheet, Image, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import Header from './src/header/header';
+import ResultHeader from './src/header/result_header';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
+import Results from './src/screens/search_results';
 
 const Stack = createStackNavigator();
+const Footer = createMaterialBottomTabNavigator();
 
-const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-  screenOptions={{
-    headerShown: false}}>
-        <Stack.Screen
-          name=" "
-          component={inicio}
+class App extends Component {
+  render(){
+    createBottomTab = () =>(
+      <Footer.Navigator
+      initialRouteName="Home"
+      activeColor="white"
+      shifting={true}
+      style={{ backgroundColor: '#212226' }}
+      >
+        <Footer.Screen 
+          name='Home'
+          component={Podcast}
+          options={{
+            tabBarLabel: 'Inicio',
+            tabBarColor:'#212226',
+            tabBarIcon: ({color}) => (
+              <Entypo name="home" size={24} color={color} />
+            ),
+          }}
         />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+        <Footer.Screen 
+          name='Random' 
+          component={Results} 
+          options={{
+            tabBarLabel: '',
+            tabBarColor:'#212226',
+            tabBarIcon: ({}) => (
+              <Image style={{width:35, height:35}}source={require('./assets/FlowPod.png')}/>
+            ),
+            }} 
+        />
+        <Footer.Screen
+          name='Generos'
+          component={Results}
+          options={{
+            tabBarLabel: 'Generos',
+            tabBarColor:'#23272a',
+            tabBarIcon: ({color}) => (
+              <FontAwesome5 name="list" size={24} color={color} />
+            ),
+          }}
+        />
+      </Footer.Navigator>
+    )
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Podcast" children={createBottomTab} options={{headerTitle: () => <Header />, headerStyle:{backgroundColor:'#363940'}}}/>
+          <Stack.Screen name="Generos" children={createBottomTab} options={{headerTitle: () => <ResultHeader />, headerStyle:{backgroundColor:'#363940'}}} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  footer:{
+    backgroundColor:'#212226',
+    width:'100%',
+    height:'100%',
+    color:'#ffff',
+  },
+});
 
 export default App;
